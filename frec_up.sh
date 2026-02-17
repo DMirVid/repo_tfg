8a30e0cd8042fe69d3ac399cecd385634c9b5a57
DMirVid <danielmironvidal@gmail.com>
2026-02-17 20:07:14 +0100

    ejecutar

--- frec_up.sh
+++ frec_up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
-echo 2200000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_min_freq > /dev/null
-echo 3000000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_max_freq > /dev/null
+echo 2100000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_min_freq > /dev/null
+echo 3100000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_max_freq > /dev/null
 
 cpupower -c 2-14:2 frequency-set -f 3000000
 cpupower -c 16-31 frequency-set -f 2200000
