718c0a6ddcc218f01b62c8c79ed14b892deb0333
Mairon243 <126103086+DMirVid@users.noreply.github.com>
2026-02-17 20:00:49 +0100

    Cambio de frecuencia

---
+++ frec_down.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cpupower -c 2-14:2 frequency-set -f 2500000
+cpupower -c 16-31 frequency-set -f 2500000
+
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
+echo 2500000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_min_freq > /dev/null
+echo 2500000 | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_max_freq > /dev/null
