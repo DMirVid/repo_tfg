80ee7291967368543975d87ce691c3c491b778d6
DMirVid <danielmironvidal@gmail.com>
2026-02-25 13:23:38 +0100

    more

--- graficar.py
+++ graficar.py
@@ -59,7 +59,7 @@ def main():
             plt.yticks(fontsize=18)
             plt.grid(True, alpha=0.3)
             plt.xlim(0, 6000)
-            plt.legend(loc='upper right', bbox_to_anchor=(1, 1), fontsize=16)
+            plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), fontsize=16, ncol=4)
             plt.tight_layout()
             
             # Guardar figura
