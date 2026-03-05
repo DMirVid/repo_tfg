26f3b90ace87a015bb3a5c293574661e9c2d9db3
Mairon243 <126103086+DMirVid@users.noreply.github.com>
2026-03-05 10:50:17 +0100

    funcionara

--- graficar_topdown.py
+++ graficar_topdown.py
@@ -90,13 +90,8 @@ def main():
                 ax2.tick_params(axis='y', labelsize=18)
                 ax2.set_ylim(0, 4)
 
-                # leyenda combinada fuera de la gráfica
-                lines1, labels1 = ax1.get_legend_handles_labels()
-                lines2, labels2 = ax2.get_legend_handles_labels()
-                plt.subplots_adjust(top=0.85)
-                fig.legend(lines1 + lines2, labels1 + labels2, 
-                          loc='upper center', bbox_to_anchor=(0.5, 1.05), 
-                          fontsize=16, ncol=5, frameon=True)
+                fig.legend(loc='upper center', bbox_to_anchor=(0.75, 1.05), 
+                          fontsize=16, ncol=5)
                 
                 plt.tight_layout()
                 
