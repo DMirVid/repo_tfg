ef72c18f053b55f2d842308cd4cf5122f32fa38f
Mairon243 <126103086+DMirVid@users.noreply.github.com>
2026-05-02 12:55:31 +0200

    energy

--- scritp.sh
+++ scritp.sh
@@ -1,20 +1,20 @@
 
-APPS = ("astar" "bwaves" "bzip2" "cactusADM" "calculix" "dealII" "gamess" "gcc" "GemsFDTD" "gobmk" "gromacs" "h264ref" "hmmer" "lbm" "leslie3d" "libquantum" "mcf" "milc" "namd" "omnetpp" "perlbench" "povray" "sjeng" "soplex" "sphinx3" "tonto" "wrf" "xalancbmk" "zeusmp" "mcf_r" "cactuBSSN_r" "namd_r" "parest_r" "povray_r" "lbm_r" "omnetpp_r" "xalancbmk_r" "x264_r" "blender_r" "deepsjeng_r" "imagick_r" "leela_r" "nab_r")
+#!/bin/bash
 
-
-> resultados.csv
+APPS=("astar" "bwaves" "bzip2" "cactusADM" "calculix" "dealII" "gamess" "gcc" "GemsFDTD" "gobmk" "gromacs" "h264ref" "hmmer" "lbm" "leslie3d" "libquantum" "mcf" "milc" "namd" "omnetpp" "perlbench" "povray" "sjeng" "soplex" "sphinx3" "tonto" "wrf" "xalancbmk" "zeusmp" "mcf_r" "cactuBSSN_r" "namd_r" "parest_r" "povray_r" "lbm_r" "omnetpp_r" "xalancbmk_r" "x264_r" "blender_r" "deepsjeng_r" "imagick_r" "leela_r" "nab_r")
+> resultados_energia.csv
 echo "APP,ENERGIA_J,TIEMPO_S" > resultados_energia.csv
 
-for app in ${APPS[@]}; do
+for app in "${APPS[@]}"; do
     
-   cat "[${app}]" >> app.yaml
+   cat "[${app}]" > app.yaml
 
     # Capturar salida de perf stat (va a stderr, por eso 2>&1)
     output=$(sudo perf stat -e power/energy-cores/ bash /home/dmirvid/python-manager/scripts/launch.bash app.yaml --ini-rep 0 --max-rep 1 2>&1)
     
-    energia=$(tail -n 5 "$output" | grep -oP '\K[0-9]+\.[0-9]+(?= Joules)')
+    energia=$(echo "$output" | grep -oP '\K[0-9]+\,[0-9]+(?= Joules)')
     
-    tiempo=$(tail -n 5 "$output" | grep -oP '\K[0-9]+\.[0-9]+(?= seconds)')
+    tiempo=$(echo "$output" | grep -oP '\K[0-9]+\,[0-9]+(?= seconds)')
     
     # Procesar los datos
     echo "$app,$energia,$tiempo" >> resultados_energia.csv
