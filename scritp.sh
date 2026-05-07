476006f3da7f0e6b21a892cb8b3e56a8d7e259e2
Mairon243 <126103086+DMirVid@users.noreply.github.com>
2026-05-07 11:14:58 +0200

    Clean

--- scritp.sh
+++ scritp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 APPS=("astar" "bwaves" "bzip2" "cactusADM" "calculix" "dealII" "gamess" "gcc" "GemsFDTD" "gobmk" "gromacs" "h264ref" "hmmer" "lbm" "leslie3d" "libquantum" "mcf" "milc" "namd" "omnetpp" "perlbench" "povray" "sjeng" "soplex" "sphinx3" "tonto" "wrf" "xalancbmk" "zeusmp" "mcf_r" "cactuBSSN_r" "namd_r" "parest_r" "povray_r" "lbm_r" "omnetpp_r" "xalancbmk_r" "x264_r" "blender_r" "deepsjeng_r" "imagick_r" "leela_r" "nab_r")
-> resultados_energia.csv
-echo "APP,ENERGIA_J,TIEMPO_S" > resultados_energia.csv
+
+echo "APP;ENERGIA_J;TIEMPO_S" > resultados_energia.csv
 
 for app in "${APPS[@]}"; do
     
@@ -11,8 +11,9 @@ for app in "${APPS[@]}"; do
     # Capturar salida de perf stat (va a stderr, por eso 2>&1)
     output=$(sudo perf stat -e power/energy-cores/ bash /home/dmirvid/python-manager/scripts/launch.bash app.yaml --ini-rep 0 --max-rep 1 2>&1)
     
-    energia=$(echo "$output" | grep -oP '\K[0-9]+\,[0-9]+(?= Joules)')
-    tiempo=$(echo "$output" | grep -oP '\K[0-9]+\,[0-9]+(?= seconds)')    
+    # Captura ambos formatos: 543,86 y 2.242,00
+    energia=$(echo "$output" | grep "Joules" | grep -oP '[0-9]{1,3}(?:\.[0-9]{3})*(?:,[0-9]+)?')
+    tiempo=$(echo "$output" | grep "seconds" | grep -oP '[0-9]{1,3}(?:\.[0-9]{3})*(?:,[0-9]+)?')    
     # Procesar los datos
     echo "$app;$energia;$tiempo" >> resultados_energia.csv
     echo "$app;$energia;$tiempo"
