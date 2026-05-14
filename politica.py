d3be93b6171188e405977fb936e472450d8abd3f
Mairon243 <126103086+DMirVid@users.noreply.github.com>
2026-05-14 12:56:41 +0200

    politica

--- politica.py
+++ politica.py
@@ -1,25 +1,24 @@
-from config import CPUS, QUANTUM_SIZE
+from config import CPUS, QUANTUM_SIZE, INSTRUCTION_COUNT_P, INSTRUCTION_COUNT_E, CYCLE_COUNT_P, CYCLE_COUNT_E
 
 # Events used by the policy
-EVENTS = ["instructions, cycles, retiring, bad_speculation, frontend_bound, backend_bound"]
+EVENTS = [INSTRUCTION_COUNT_P, INSTRUCTION_COUNT_E, CYCLE_COUNT_P, CYCLE_COUNT_E]
 
-
-def asignacion_core(nucleo):
-    
-
+# Procesos con mayot IPC se mueven a los P cores
 def schedule(processes):
-    # WARNING: ONLY WORKS WITH 4 PROCESSES
     
-    # inatrucciones por quantum
-    ips = []
+    ipc = {}
     for proc in processes:
-        ips.append(proc.event_counts['instructions'] / QUANTUM_SIZE)
+        ipc[proc] = 0
+        if proc.cores < 16:
+            ipc[proc] = proc.event_counts[INSTRUCTION_COUNT_P] / proc.event_counts[CYCLE_COUNT_P]
+        else:
+            ipc[proc] = proc.event_counts[INSTRUCTION_COUNT_E] / proc.event_counts[CYCLE_COUNT_E]
 
-    # Sort processes by memory load count (lowest to highest)
-    sorted_procs = sorted(processes, key=lambda proc: proc.event_counts[''])
+    sorted_procs = sorted(processes, key=lambda proc: ipc[proc])
 
-    # Set affinity balancing load number
-    sorted_procs[0].set_affinity({CPUS[0][0]})  # Lowest #loads in core 0, context 0
-    sorted_procs[3].set_affinity({CPUS[0][1]})  # Highest #loads in core 0, context 1
-    sorted_procs[1].set_affinity({CPUS[1][0]})  # Next-to-lowest in core 1, context 0
-    sorted_procs[2].set_affinity({CPUS[1][1]})  # Next-to-highest in core 1, context 1
+    for i in range(len(sorted_procs)):
+        if i < 4:
+            sorted_procs[i].set_affinity({CPUS[i][0]}) 
+        else:
+            sorted_procs[i].set_affinity({CPUS[8+i*2][0]})
+   
