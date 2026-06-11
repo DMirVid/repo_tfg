fb19ab578f6b7f0ffebd5d233bf907e942bed9d3
DMirVid <danielmironvidal@gmail.com>
2026-06-11 17:38:08 +0200

    fo

--- ipc_bajo.py
+++ ipc_bajo.py
@@ -33,7 +33,7 @@ def schedule(processes, quantum=0):
             ipc_history[i] = 1
 
         ipc_change_significant = False
-        if quantum > 0 and i in ipc_history and ipc_history[i] > 0:
+        if quantum > 0:
             percent_change = abs((ipc - ipc_history[i]) / ipc_history[i]) * 100
             ipc_change_significant = percent_change > 10
         
